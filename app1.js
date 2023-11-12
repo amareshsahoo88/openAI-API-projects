872e0974792c59cea1f91bfa045e92a7a15fc342
amaresh sahoo <112602756+amareshsahoo88@users.noreply.github.com>
2023-11-12 10:55:25 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,23 @@
+const API_KEY = "Your API key";
+async function fetchData() {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "gpt-4",
+            messages: [
+                {
+                    role: "user",
+                    content: "Hello!"
+                }
+            ]
+
+        })
+    })
+    const data = await response.json()
+    console.log(data)
+}
+fetchData();
